9c0658ad45c57e1e1b7401733f7a1b6c6409df08
hanxi27 <hanxi1999@gmail.com>
2024-06-30 22:38:23 +0800

    hh

--- inno_store/lib/main.dart
+++ inno_store/lib/main.dart
@@ -1,31 +1,48 @@
-// lib/main.dart
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'first.dart'; // Import the first.dart file
-import 'login.dart'; // Import the login.dart file
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:inno_store/features/app/splash_screen/splash_screen.dart';
+import 'package:inno_store/features/user_auth/presentations/pages/home_page.dart';
+import 'package:inno_store/features/user_auth/presentations/pages/login_page.dart';
+import 'package:inno_store/features/user_auth/presentations/pages/sign_up_page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyAFDPwZCsNSnckcq2jHvJcm7ejEhg5-2LU",
+        appId: "1:396405032929:android:fa7a9d2db91884af1434a0",
+        messagingSenderId: "396405032929",
+        projectId: "smart-retail-app-4b6a5",
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Navigation Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/', // Set SplashScreen as the initial route
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
       routes: {
-        '/': (context) => const SplashScreen(), // Show SplashScreen first
-        '/login': (context) => const LoginPage(), // Show LoginPage after SplashScreen
-        // Define other routes as needed
-        // '/home': (context) => HomePage(),
-        // '/profile': (context) => const ProfilePage(username: '',),
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
       },
     );
   }
+  
+  
 }
