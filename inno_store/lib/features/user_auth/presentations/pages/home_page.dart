af27f0e0fc79554b99880db6365a210bd2c56ddb
hanxi27 <hanxi1999@gmail.com>
2024-07-01 15:43:45 +0800

    f

--- inno_store/lib/features/user_auth/presentations/pages/home_page.dart
+++ inno_store/lib/features/user_auth/presentations/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import '../../../../global/common/toast.dart';
 
@@ -15,115 +16,256 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    UserHomeScreen(),
+    MapScreen(),
+    RequestHelpScreen(),
+    AccountScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text("HomePage"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  _createData(UserModel(
-                    username: "Henry",
-                    age: 21,
-                    address: "London",
-                  ));
-                },
-                child: Container(
-                  height: 45,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Center(
-                    child: Text(
-                      "Create Data",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              StreamBuilder<List<UserModel>>(
-                stream: _readData(),
-                builder: (context, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.waiting){
-                    return Center(child: CircularProgressIndicator(),);
-                  } if(snapshot.data!.isEmpty){
-                    return Center(child:Text("No Data Yet"));
-                  }
-                  final users = snapshot.data;
-                  return Padding(padding: EdgeInsets.all(8),
-                  child: Column(
-                    children: users!.map((user) {
-                      return ListTile(
-                        leading: GestureDetector(
-                          onTap: (){
-                            _deleteData(user.id!);
-                          },
-                          child: Icon(Icons.delete),
-                        ),
-                        trailing: GestureDetector(
-                          onTap: (){
-                            _updateData(
-                              UserModel(
-                                id: user.id,
-                                username: "John Wick",
-                                address: "Pakistan",)
-                            );
-                          },
-                          child: Icon(Icons.update),
-                        ),
-                        title: Text(user.username!),
-                        subtitle: Text(user.address!),
-                      );
-                    }).toList()
-                  ),);
-                }
-              ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Inno Store'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: 'Request Help',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'My Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
 
-              GestureDetector(
-                onTap: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushNamed(context, "/login");
-                  showToast(message: "Successfully signed out");
-                },
-                child: Container(
-                  height: 45,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Center(
-                    child: Text(
-                      "Sign out",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ),
+class UserHomeScreen extends StatefulWidget {
+  const UserHomeScreen({super.key});
+
+  @override
+  State<UserHomeScreen> createState() => _UserHomeScreenState();
+}
+
+class _UserHomeScreenState extends State<UserHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> imgList = [
+      'https://via.placeholder.com/600x400?text=Buy+1+Free+1',
+      'https://via.placeholder.com/600x400?text=20%+Off',
+      'https://via.placeholder.com/600x400?text=Free+Shipping',
+    ];
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Hello, User!',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              )
+                IconButton(
+                  icon: Icon(Icons.account_circle, size: 40),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.pink,
+            padding: EdgeInsets.all(16),
+            margin: EdgeInsets.symmetric(vertical: 8.0),
+            child: Column(
+              children: [
+                Text(
+                  'Kaw Kaw Deals',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  '30% OFF\n27 Jun - 1 Jul 2024',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+              enlargeCenterPage: true,
+            ),
+            items: imgList.map((item) => Container(
+              child: Center(
+                child: Image.network(item, fit: BoxFit.cover, width: 1000)
+              ),
+            )).toList(),
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            crossAxisCount: 4,
+            children: [
+              _buildIconCard(Icons.local_offer, 'Kaw Kaw Deals'),
+              _buildIconCard(Icons.health_and_safety, 'Health'),
+              _buildIconCard(Icons.person, 'Personal Care'),
+              _buildIconCard(Icons.spa, 'Skincare'),
+              _buildIconCard(Icons.brush, 'Cosmetics'),
+              _buildIconCard(Icons.card_giftcard, 'Coupon Zone'),
+              _buildIconCard(Icons.local_hospital, 'Health 45+'),
+              _buildIconCard(Icons.face, 'Beauty Profile'),
+              _buildIconCard(Icons.eco, 'Sustainable Choices'),
+              _buildIconCard(Icons.content_cut, 'Haircare Finder'),
             ],
           ),
-        ));
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _createData(UserModel(
+                      username: "Henry",
+                      age: 21,
+                      address: "London",
+                    ));
+                  },
+                  child: Container(
+                    height: 45,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text(
+                        "Create Data",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                StreamBuilder<List<UserModel>>(
+                  stream: _readData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.data!.isEmpty) {
+                      return Center(child: Text("No Data Yet"));
+                    }
+                    final users = snapshot.data;
+                    return Column(
+                      children: users!.map((user) {
+                        return ListTile(
+                          leading: GestureDetector(
+                            onTap: () {
+                              _deleteData(user.id!);
+                            },
+                            child: Icon(Icons.delete),
+                          ),
+                          trailing: GestureDetector(
+                            onTap: () {
+                              _updateData(
+                                UserModel(
+                                  id: user.id,
+                                  username: "John Wick",
+                                  address: "Pakistan",
+                                ),
+                              );
+                            },
+                            child: Icon(Icons.update),
+                          ),
+                          title: Text(user.username!),
+                          subtitle: Text(user.address!),
+                        );
+                      }).toList(),
+                    );
+                  },
+                ),
+                GestureDetector(
+                  onTap: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.pushNamed(context, "/login");
+                    showToast(message: "Successfully signed out");
+                  },
+                  child: Container(
+                    height: 45,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text(
+                        "Sign out",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Stream<List<UserModel>> _readData(){
+  Widget _buildIconCard(IconData icon, String label) {
+    return Column(
+      children: [
+        Icon(icon, size: 40),
+        SizedBox(height: 8),
+        Text(label, textAlign: TextAlign.center),
+      ],
+    );
+  }
+
+  Stream<List<UserModel>> _readData() {
     final userCollection = FirebaseFirestore.instance.collection("users");
 
-    return userCollection.snapshots().map((qureySnapshot)
-    => qureySnapshot.docs.map((e)
-    => UserModel.fromSnapshot(e),).toList());
+    return userCollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList());
   }
 
   void _createData(UserModel userModel) {
@@ -135,7 +277,7 @@ class _HomePageState extends State<HomePage> {
       username: userModel.username,
       age: userModel.age,
       address: userModel.address,
-        id: id,
+      id: id,
     ).toJson();
 
     userCollection.doc(id).set(newUser);
@@ -152,28 +294,24 @@ class _HomePageState extends State<HomePage> {
     ).toJson();
 
     userCollection.doc(userModel.id).update(newData);
-
   }
 
   void _deleteData(String id) {
     final userCollection = FirebaseFirestore.instance.collection("users");
 
     userCollection.doc(id).delete();
-
   }
-
 }
 
-class UserModel{
+class UserModel {
   final String? username;
   final String? address;
   final int? age;
   final String? id;
 
-  UserModel({this.id,this.username, this.address, this.age});
+  UserModel({this.id, this.username, this.address, this.age});
 
-
-  static UserModel fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot){
+  static UserModel fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot) {
     return UserModel(
       username: snapshot['username'],
       address: snapshot['address'],
@@ -182,12 +320,39 @@ class UserModel{
     );
   }
 
-  Map<String, dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     return {
       "username": username,
       "age": age,
       "id": id,
       "address": address,
     };
+  }
+}
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Map Screen'),
+    );
+  }
+}
+
+class RequestHelpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Request Help Screen'),
+    );
+  }
+}
+
+class AccountScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('My Account Screen'),
+    );
   }
 }
