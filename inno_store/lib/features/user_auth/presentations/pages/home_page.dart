c4587e8939c6bc9d515f50c5050cb9f795ee040b
hanxi27 <hanxi1999@gmail.com>
2024-07-01 16:22:05 +0800

    i

--- inno_store/lib/features/user_auth/presentations/pages/home_page.dart
+++ inno_store/lib/features/user_auth/presentations/pages/home_page.dart
@@ -1,25 +1,18 @@
-import 'dart:ffi';
+// lib/home.dart
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-import '../../../../global/common/toast.dart';
-
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   static List<Widget> _widgetOptions = <Widget>[
-    UserHomeScreen(),
+    HomeScreen(),
     MapScreen(),
     RequestHelpScreen(),
     AccountScreen(),
@@ -35,7 +28,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         title: Text('Inno Store'),
       ),
       body: _widgetOptions.elementAt(_selectedIndex),
@@ -66,22 +58,15 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class UserHomeScreen extends StatefulWidget {
-  const UserHomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  final List<String> imgList = [
+    'https://via.placeholder.com/600x400?text=Buy+1+Free+1',
+    'https://via.placeholder.com/600x400?text=20%+Off',
+    'https://via.placeholder.com/600x400?text=Free+Shipping',
+  ];
 
-  @override
-  State<UserHomeScreen> createState() => _UserHomeScreenState();
-}
-
-class _UserHomeScreenState extends State<UserHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final List<String> imgList = [
-      'https://via.placeholder.com/600x400?text=Buy+1+Free+1',
-      'https://via.placeholder.com/600x400?text=20%+Off',
-      'https://via.placeholder.com/600x400?text=Free+Shipping',
-    ];
-
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -151,101 +136,6 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
               _buildIconCard(Icons.content_cut, 'Haircare Finder'),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    _createData(UserModel(
-                      username: "Henry",
-                      age: 21,
-                      address: "London",
-                    ));
-                  },
-                  child: Container(
-                    height: 45,
-                    width: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Center(
-                      child: Text(
-                        "Create Data",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                StreamBuilder<List<UserModel>>(
-                  stream: _readData(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.data!.isEmpty) {
-                      return Center(child: Text("No Data Yet"));
-                    }
-                    final users = snapshot.data;
-                    return Column(
-                      children: users!.map((user) {
-                        return ListTile(
-                          leading: GestureDetector(
-                            onTap: () {
-                              _deleteData(user.id!);
-                            },
-                            child: Icon(Icons.delete),
-                          ),
-                          trailing: GestureDetector(
-                            onTap: () {
-                              _updateData(
-                                UserModel(
-                                  id: user.id,
-                                  username: "John Wick",
-                                  address: "Pakistan",
-                                ),
-                              );
-                            },
-                            child: Icon(Icons.update),
-                          ),
-                          title: Text(user.username!),
-                          subtitle: Text(user.address!),
-                        );
-                      }).toList(),
-                    );
-                  },
-                ),
-                GestureDetector(
-                  onTap: () {
-                    FirebaseAuth.instance.signOut();
-                    Navigator.pushNamed(context, "/login");
-                    showToast(message: "Successfully signed out");
-                  },
-                  child: Container(
-                    height: 45,
-                    width: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Center(
-                      child: Text(
-                        "Sign out",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
@@ -259,74 +149,6 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
         Text(label, textAlign: TextAlign.center),
       ],
     );
-  }
-
-  Stream<List<UserModel>> _readData() {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    return userCollection.snapshots().map((querySnapshot) =>
-        querySnapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList());
-  }
-
-  void _createData(UserModel userModel) {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    String id = userCollection.doc().id;
-
-    final newUser = UserModel(
-      username: userModel.username,
-      age: userModel.age,
-      address: userModel.address,
-      id: id,
-    ).toJson();
-
-    userCollection.doc(id).set(newUser);
-  }
-
-  void _updateData(UserModel userModel) {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    final newData = UserModel(
-      username: userModel.username,
-      id: userModel.id,
-      address: userModel.address,
-      age: userModel.age,
-    ).toJson();
-
-    userCollection.doc(userModel.id).update(newData);
-  }
-
-  void _deleteData(String id) {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    userCollection.doc(id).delete();
-  }
-}
-
-class UserModel {
-  final String? username;
-  final String? address;
-  final int? age;
-  final String? id;
-
-  UserModel({this.id, this.username, this.address, this.age});
-
-  static UserModel fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot) {
-    return UserModel(
-      username: snapshot['username'],
-      address: snapshot['address'],
-      age: snapshot['age'],
-      id: snapshot['id'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      "username": username,
-      "age": age,
-      "id": id,
-      "address": address,
-    };
   }
 }
 
