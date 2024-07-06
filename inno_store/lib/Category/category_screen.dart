5b0e64bb9cf5766ca4437d3ccbc236c160552f3b
Xin0620 <hanwenxin113@gmail.com>
2024-07-06 14:55:49 +0800

    category1

--- inno_store/lib/Category/category_screen.dart
+++ inno_store/lib/Category/category_screen.dart
@@ -29,30 +29,66 @@ class _CategoryScreenState extends State<CategoryScreen> {
     'Health Care': [
       {"title": "Swisse Ultiboost Vitamin C + Manuka Honey 120 Tablets", "image": Assets.vitaminc},
       {"title": "Swisse Ultiboost High Strength Krill Oil 30 Capsules", "image": Assets.krilloil},
+      {"title": "Blackmores Digestive Enzymes Plus Capsule 60s", "image": Assets.enzymesplus},
+      {"title": "Blackmores Bio Ace Plus Capsule 30s", "image": Assets.bioaceplus},
+      {"title": "Blackmores Bio Zinc Capsule 168s", "image": Assets.biozinc},
+      {"title": "Blackmores Buffered C Capsule 30s", "image": Assets.bufferedc},
+      {"title": "EYS Bird's Nest With Rock Sugar", "image": Assets.birdnest},
+      {"title": "EYS Pure Chicken Essence", "image": Assets.chickenessence},
+      {"title": "HM Euphoria Longana Honey", "image": Assets.honey},
+      {"title": "Brand's Essence of Chicken 70g x 30's", "image": Assets.brands},
     ],
-    // other categories...
 
     'Groceries': [
       {"title": "Nestle Koko Krunch 450g", "image": Assets.kokokrunch},
-      // add more products
+      {"title": "Saji Brand Cooking Oil 5kg", "image": Assets.saji},
+      {"title": "Knife Cooking Oil 5kg", "image": Assets.knifeoil},
+      {"title": "Maggi Curry Flavour Instant Noodles 79g x 5", "image": Assets.maggiekari},
+      {"title": "Old Town 2 in 1 White Coffee (Coffee & Creamer) 375g", "image": Assets.oldtown},
+      {"title": "Old Town 3-in-1 Hazelnut Instant White Coffee 570g", "image": Assets.oldtownhazelnut},
+      {"title": "Yeo's Chicken Curry with Potatoes 280g", "image": Assets.yeoscurry},
+      {"title": "Jasmine Super 5 White Rice Imported 5kg", "image": Assets.jasmine},
+      {"title": "Ayam Brand Mackerel in Tomato Sauce 230g", "image": Assets.tomatosauce},
+      {"title": "Cap Rambutan 5% Super Import White Rice 5kg", "image": Assets.rambutans},
+      {"title": "Milo Original Chocolate Malt Drink 30g x 18", "image": Assets.milo},
+      {"title": "Nestle Kit Kat Sharebag Value Pack 17g x 24", "image": Assets.kitkat},
+      {"title": "Milo Soft Pack 2kg", "image": Assets.milo2kg},
     ],
-
 
     'Make Up': [
+      {"title": "Garnier Skin Naturals Micellar Water Pink 125ml", "image": Assets.garnier},
+      {"title": "Reihaku Hatomugi Whip Face Wash Cleansing Water (Make Up Remover) 200ml", "image": Assets.hatomugi},
+      {"title": "Garnier Micellar Salicylic BHA 125ml", "image": Assets.garnierblue},
+      {"title": "Silky White Bright Up Liquid Foundation 01 Light", "image": Assets.foundation},
+      {"title": "Palladio Herbal Foundation Tube PFS01 Ivory", "image": Assets.palladio},
+      {"title": "Palladio Herbal Foundation Tube PFS02 Porcelain", "image": Assets.palladio02},
+      {"title": "Palladio Dual Wet & Dry Foundation WD400 Laurel Nude", "image": Assets.dryfoundation},
+      {"title": "Palladio Dual Wet & Dry Foundation WD401 Ivory Myrrh", "image": Assets.dryfoundation401},
+      {"title": "Rimmel Wonder'Ink Ultimate Waterproof Eyeliner", "image": Assets.eyeliner},
+      {"title": "SilkyGirl Long-Wearing Eyeliner 01 Black Black", "image": Assets.eyelinerblack},
     
-      // add more products
     ],
     'Pets Care': [
+     {"title": "Dog Dry Food Chicken & Veg 3kg", "image": Assets.pedigree},
+     {"title": "Cat Wet Food Flake Tuna in Gravy 85gm", "image": Assets.sheba},
+     {"title": "Dog Oral Care Dentastix Toy 60g", "image": Assets.dogoral},
+     {"title": "Pottty Here Training Aid Spray 8Oz", "image": Assets.naturvet},
+     {"title": "Ear Wash Liquid 4Oz", "image": Assets.earwash},
+     {"title": "Peony Anti-Bacteria Formula Pets Shampoo 400ml", "image": Assets.shampoo},
+     {"title": "Unique Pet Shower Silicon Brush with Soap Container", "image": Assets.brush},
      
-      // add more products
     ],
-    'Household and Lifestyle': [
-     
-      // add more products
-    ],
+
     'Hair Care': [
-   
-      // add more products
+      {"title": "KUNDAL Honey & Macadamia Nature Shampoo - Cherry Blossom 500ml", "image": Assets.kundalshampoo},
+      {"title": "Kundal Honey & Macadamia Hair Treatment - Cherry Blossom 500ml", "image": Assets.kundalhoney},
+      {"title": "Grafen Perfume Hair Shampoo Emerald Blossom 500ml (Anti-Hair Loss)", "image": Assets.grafen},
+      {"title": "Ryo Hair Loss Expert Scalp Massage Essence 80ml", "image": Assets.ryo},
+      {"title": "Kundal Caffeine Scalp Care Tonic 100ml", "image": Assets.hairtonic},
+      {"title": "Pantene 3Mm Conditioner 480Ml Keratin Silky Smooth", "image": Assets.pantene},
+      {"title": "Amino Mason Treatment Night Recipe Sleek 450Ml", "image": Assets.amino},   
+      {"title": "Ryo Damage Care And Nourishing Shampoo 480ml", "image": Assets.ryodamage},   
+    
     ],
   };
 
@@ -87,7 +123,6 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   buildCategoryItem('Groceries', Icons.shopping_cart),
                   buildCategoryItem('Make Up', Icons.brush),
                   buildCategoryItem('Pets Care', Icons.pets),
-                  buildCategoryItem('Household and Lifestyle', Icons.home),
                   buildCategoryItem('Hair Care', Icons.face),
                 ],
               ),
@@ -116,9 +151,9 @@ class _CategoryScreenState extends State<CategoryScreen> {
                     padding: const EdgeInsets.all(10.0),
                     gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                       maxCrossAxisExtent: 200,
-                      childAspectRatio: 3 / 2,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
+                      childAspectRatio: 2/3,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
                     ),
                     itemCount: filteredProducts.length,
                     itemBuilder: (ctx, i) => ProductItem(
--- inno_store/lib/assets.dart
+++ inno_store/lib/assets.dart
@@ -6,5 +6,77 @@ class Assets {
   //Healthcare
   static const String vitaminc = 'assets/products/healthcare/vitaminc.jpg';
   static const String krilloil = 'assets/products/healthcare/krilloil.png';
+  static const String enzymesplus = 'assets/products/healthcare/enzymesplus.jpg';
+  static const String bioaceplus = 'assets/products/healthcare/bioaceplus.jpg';
+  static const String biozinc = 'assets/products/healthcare/biozinc.jpg';
+  static const String bufferedc = 'assets/products/healthcare/bufferedc.jpg';
+  static const String birdnest = 'assets/products/healthcare/birdnest.jpg';
+  static const String chickenessence = 'assets/products/healthcare/chickenessence.jpg';
+  static const String honey = 'assets/products/healthcare/honey.jpg';
+  static const String brands = 'assets/products/healthcare/brands.jpg';
+  
+  //Groceries
   static const String kokokrunch = 'assets/products/groceries/kokokrunch.jpg';
+  static const String saji = 'assets/products/groceries/saji.jpg';
+  static const String knifeoil = 'assets/products/groceries/knifeoil.jpg';
+  static const String maggiekari = 'assets/products/groceries/maggiekari.jpg';
+  static const String oldtown = 'assets/products/groceries/oldtown.jpg';
+  static const String oldtownhazelnut = 'assets/products/groceries/oldtownhazelnut.jpg';
+  static const String yeoscurry = 'assets/products/groceries/yeoscurry.jpg';
+  static const String jasmine = 'assets/products/groceries/jasmine.jpg';
+  static const String tomatosauce = 'assets/products/groceries/tomatosauce.jpg';
+  static const String rambutans = 'assets/products/groceries/rambutan.jpg';
+  static const String milo = 'assets/products/groceries/milo.jpg';
+  static const String kitkat = 'assets/products/groceries/kitkat.jpg';
+  static const String milo2kg = 'assets/products/groceries/milo2kg.jpg';
+
+  //Make Up
+  static const String garnier = 'assets/products/makeup/garnier.jpg';
+  static const String hatomugi = 'assets/products/makeup/hatomugi.jpg';
+  static const String garnierblue = 'assets/products/makeup/garnierblue.jpg';
+  static const String foundation = 'assets/products/makeup/foundation.jpg';
+  static const String palladio = 'assets/products/makeup/palladio.jpg';
+  static const String palladio02 = 'assets/products/makeup/palladio02.jpg';
+  static const String dryfoundation = 'assets/products/makeup/dryfoundation.jpg';
+  static const String dryfoundation401 = 'assets/products/makeup/dryfoundation401.jpg';
+  static const String eyeliner = 'assets/products/makeup/eyeliner.jpg';
+  static const String eyelinerblack = 'assets/products/makeup/eyelinerblack.jpg';
+
+  //petcare
+  static const String pedigree = 'assets/products/petcare/pedigree.jpg';
+  static const String sheba = 'assets/products/petcare/sheba.jpg';
+  static const String dogoral = 'assets/products/petcare/dogoral.jpg';
+  static const String whiskas = 'assets/products/petcare/whiskas.jpg';
+  static const String naturvet = 'assets/products/petcare/naturvet.jpg';
+  static const String earwash = 'assets/products/petcare/earwash.jpg';
+  static const String shampoo = 'assets/products/petcare/shampoo.jpg';
+  static const String brush = 'assets/products/petcare/brush.jpg';
+  static const String pet = 'assets/products/petcare/pet.jpg';
+
+  //haircare
+  static const String kundalshampoo = 'assets/products/haircare/kundalshampoo.jpg';
+  static const String kundalhoney = 'assets/products/haircare/kundalhoney.jpg';
+  static const String grafen = 'assets/products/haircare/grafen.jpg';
+  static const String ryo = 'assets/products/haircare/ryo.jpg';
+  static const String hairtonic = 'assets/products/haircare/hairtonic.jpg';
+  static const String pantene = 'assets/products/haircare/pantene.jpg';
+  static const String amino = 'assets/products/haircare/amino.jpg';
+  static const String ryodamage = 'assets/products/haircare/ryodamage.jpg';
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
 }
